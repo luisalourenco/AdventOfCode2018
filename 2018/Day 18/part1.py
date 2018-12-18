618e3687ef8a4bb7d53da73e76e70ef22b3554ed
luisalourenco <kikentai@gmail.com>
2018-12-18 23:21:24 +0000

    Buggy day 18

--- 2018/Day 18/part1.py
+++ 2018/Day 18/part1.py
@@ -11,11 +11,53 @@ def print_map(m):
 # i,j  #
 #  #  #
 '''
-def countTrees(i, j, area):	
-	return int(area[i-1][j] == "|") + int(area[i-1][j-1] == "|") + int(area[i-1][j+1] == "|")+ int(area[i][j-1] == "|") + int(area[i][j+1] == "|")	+ int(area[i+1][j-1] == "|") + int(area[i+1][j] == "|") + int(area[i+1][j+1] == "|")
+def countTrees(i, j, area):
+	count = 0
+	size = len(area[i]) - 1
+
+	if i == 0 and j == size:
+		count = int(area[i][j-1] == "|") + int(area[i+1][j-1] == "|") + int(area[i+1][j] == "|") 
+	elif i == size and j == 0:
+		count = int(area[i-1][j] == "|") + int(area[i-1][j+1] == "|") + int(area[i][j+1] == "|")
+	elif i == 0 and j > 0 and j < size:
+		count = int(area[i][j-1] == "|") + int(area[i][j+1] == "|")	+ int(area[i+1][j-1] == "|") + int(area[i+1][j] == "|") + int(area[i+1][j+1] == "|")
+	elif i == 0 and j == 0 and j < size:
+		count = int(area[i][j+1] == "|") + int(area[i+1][j] == "|") + int(area[i+1][j+1] == "|")
+	elif i > 0 and j == 0 and i < size:
+		count =  int(area[i][j+1] == "|") + int(area[i+1][j] == "|") + int(area[i+1][j+1] == "|") + int(area[i-1][j] == "|") + int(area[i-1][j+1] == "|") + int(area[i][j+1] == "|") + int(area[i+1][j] == "|") + int(area[i+1][j+1] == "|")
+	elif i == size and j == size:
+		count = int(area[i-1][j] == "|") + int(area[i-1][j-1] == "|") + int(area[i][j-1] == "|")
+	elif i == size and j < size and j > 0:
+		count = int(area[i-1][j] == "|") + int(area[i-1][j-1] == "|") + int(area[i-1][j+1] == "|")+ int(area[i][j-1] == "|") + int(area[i][j+1] == "|")	
+	elif j == size and i < size and i > 0:
+		count = int(area[i-1][j] == "|") + int(area[i-1][j-1] == "|") + int(area[i][j-1] == "|") + int(area[i+1][j-1] == "|") + int(area[i+1][j] == "|")
+	else:
+		count = int(area[i-1][j] == "|") + int(area[i-1][j-1] == "|") + int(area[i-1][j+1] == "|")+ int(area[i][j-1] == "|") + int(area[i][j+1] == "|")	+ int(area[i+1][j-1] == "|") + int(area[i+1][j] == "|") + int(area[i+1][j+1] == "|")
+	return count
+
 
 def countLumberyards(i, j, area):	
-	return int(area[i-1][j] == "#") + int(area[i-1][j-1] == "#") + int(area[i-1][j+1] == "#")+ int(area[i][j-1] == "#") + int(area[i][j+1] == "#")	+ int(area[i+1][j-1] == "#") + int(area[i+1][j] == "#") + int(area[i+1][j+1] == "#")
+	count = 0
+	size = len(area[i]) - 1
+	if i == 0 and j == size:
+		count = int(area[i][j-1] == "#") + int(area[i+1][j-1] == "#") + int(area[i+1][j] == "#") 
+	elif i == size and j == 0:
+		count = int(area[i-1][j] == "#") + int(area[i-1][j+1] == "#") + int(area[i][j+1] == "#")
+	elif i == 0 and j > 0 and j < size:
+		count = int(area[i][j-1] == "#") + int(area[i][j+1] == "#")	+ int(area[i+1][j-1] == "#") + int(area[i+1][j] == "#") + int(area[i+1][j+1] == "#")
+	elif i == 0 and j == 0 and j < size:
+		count = int(area[i][j+1] == "#") + int(area[i+1][j] == "#") + int(area[i+1][j+1] == "#")
+	elif i > 0 and j == 0 and i < size:
+		count =  int(area[i][j+1] == "#") + int(area[i+1][j] == "#") + int(area[i+1][j+1] == "#") + int(area[i-1][j] == "#") + int(area[i-1][j+1] == "#") + int(area[i][j+1] == "#") + int(area[i+1][j] == "#") + int(area[i+1][j+1] == "#")
+	elif i == size and j == size:
+		count = int(area[i-1][j] == "#") + int(area[i-1][j-1] == "#") + int(area[i][j-1] == "#")
+	elif i == size and j < size and j > 0:
+		count = int(area[i-1][j] == "#") + int(area[i-1][j-1] == "#") + int(area[i-1][j+1] == "#")+ int(area[i][j-1] == "#") + int(area[i][j+1] == "#")	
+	elif j == size and i < size and i > 0:
+		count = int(area[i-1][j] == "#") + int(area[i-1][j-1] == "#") + int(area[i][j-1] == "#") + int(area[i+1][j-1] == "#") + int(area[i+1][j] == "#")
+	else:
+		count = int(area[i-1][j] == "#") + int(area[i-1][j-1] == "#") + int(area[i-1][j+1] == "#")+ int(area[i][j-1] == "#") + int(area[i][j+1] == "#")	+ int(area[i+1][j-1] == "#") + int(area[i+1][j] == "#") + int(area[i+1][j+1] == "#")
+	return count
 
 
 i = 0
@@ -36,19 +78,36 @@ with open(filepath) as fp:
 	
 	result = [ ['.'] * s for n in range(s)]
 	for _ in range(10):
-		for i in range(1,s-1):
-			for j in range(1,s-1):
+	
+		for i in range(s):
+			for j in range(s):
+				#print "checking area ",i,",",j,": ", area[i][j]
 				if area[i][j] == "." and countTrees(i, j, area) >= 3:
+					#print "Grow Tree!"
 					result[i][j] = "|"  
-				if area[i][j] == "|" and countLumberyards(i, j, area) >= 3:
+				elif area[i][j] == "|" and countLumberyards(i, j, area) >= 3:
+					#print "Become Lumberyard!"
 					result[i][j] = "#" 
-				if area[i][j] == "#" and countLumberyards(i, j, area) >= 1 and countTrees(i, j, area) >= 1:
+				elif area[i][j] == "#" and countLumberyards(i, j, area) >= 1 and countTrees(i, j, area) >= 1:
+					#print "Remain lumberyard"
 					result[i][j] = "#"
-				else:
+				elif area[i][j] == "#" and (countLumberyards(i, j, area) < 1 or countTrees(i, j, area) < 1):
+					#print "Become empty!"
 					result[i][j] = "."
 		#end for
-		area = result
+		import copy
+		area = copy.deepcopy(result)
+		#result = [ ['.'] * s for n in range(s)]
 	#end for
-	print_map(area)
+	trees = 0
+	lumb = 0
+	for s in area:
+		lumb += s.count("#")
+		trees += s.count("|")
+
+	print_map(result)
+	print trees
+	print lumb
+	print trees * lumb
 
 	
