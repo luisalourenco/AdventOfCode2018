31422a54fb521ed3a210b320e5a4945597ac5fac
Luisa Lourenço <luisa.lourenco@outsystems.com>
2018-12-18 13:09:15 +0000

    More stuff

--- 2018/Day 18/part1.py
+++ 2018/Day 18/part1.py
@@ -3,8 +3,19 @@ filepath = 'test.txt'
 def print_map(m):
 	i = 0
 	for s in m:
-		print i,": ", s
+		print (i,": ", "".join(s))
 		i += 1	
+
+'''
+#  #   #
+# i,j  #
+#  #  #
+'''
+def countTrees(i, j, area):	
+	return int(area[i-1][j] == "|") + int(area[i-1][j-1] == "|") + int(area[i-1][j+1] == "|")+ int(area[i][j-1] == "|") + int(area[i][j+1] == "|")	+ int(area[i+1][j-1] == "|") + int(area[i+1][j] == "|") + int(area[i+1][j+1] == "|")
+
+def countLumberyards(i, j, area):	
+	return int(area[i-1][j] == "#") + int(area[i-1][j-1] == "#") + int(area[i-1][j+1] == "#")+ int(area[i][j-1] == "#") + int(area[i][j+1] == "#")	+ int(area[i+1][j-1] == "#") + int(area[i+1][j] == "#") + int(area[i+1][j+1] == "#")
 
 
 i = 0
@@ -20,20 +31,24 @@ with open(filepath) as fp:
 		data = fp.readline().strip()
 
 	print_map(area)
-
+	#print(area[1][6])
+	#print(countLumberyards(1,6, area))
+	
 	result = [ ['.'] * s for n in range(s)]
-
 	for _ in range(10):
 		for i in range(1,s-1):
 			for j in range(1,s-1):
-				if area[i][j] == "." and countTrees(i, j) >= 3:
+				if area[i][j] == "." and countTrees(i, j, area) >= 3:
 					result[i][j] = "|"  
-				if area[i][j] == "|" and countLumberyards(i, j) >= 3:
+				if area[i][j] == "|" and countLumberyards(i, j, area) >= 3:
 					result[i][j] = "#" 
-				if area[i][j] == "#" and countLumberyards(i, j) >= 1 and countTrees(i, j) >= 1:
+				if area[i][j] == "#" and countLumberyards(i, j, area) >= 1 and countTrees(i, j, area) >= 1:
 					result[i][j] = "#"
 				else:
 					result[i][j] = "."
 		#end for
 		area = result
 	#end for
+	print_map(area)
+
+	
